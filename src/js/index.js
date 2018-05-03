5aabd17611c7af340e5632bbe0bbf8d51466d95a
Yuliia Khorkova <ykhorkova@gmail.com>
2018-05-03 00:36:04 +0000

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,33 @@ import 'bootstrap';
 //include your index.scss file into the bundle
 import '../styles/index.scss';
 
-//import your own components
-import {Home} from './component/home.jsx';
+function Home(){
+    return (
+        <div className="card-body mx-auto">
+            <h1>todos</h1>
+            <div className="card">
+                <div className="card-header">
+            What needs to be done?
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Make the bed</li>
+                    <li className="list-group-item">Walk the god</li>
+                    <li className="list-group-item">Prepare a dinner</li>
+                </ul>
+            </div>
+        </div>);
+}
 
-//render your react application
-ReactDOM.render(
-    <Home />,
-    document.querySelector('#app')
-);
+
+
+
+
+ReactDOM.render(<Home />,document.querySelector('#app'));
+
+
+
+
+// //import your own components
+// import {Home} from './component/home.jsx';
+
+// //render your react application
